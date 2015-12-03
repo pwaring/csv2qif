ea7d48f0b7f889cd4584c85c0043a97dc35352c3
Paul Waring <paul@xk7.net>
2015-12-03 11:32:46 +0000

    First working version of script

--- csv2qif.pl
+++ csv2qif.pl
@@ -1,7 +1,46 @@
 #!/usr/bin/env perl
 
+use 5.014;
+
 use strict;
 use warnings;
 use autodie;
 
+use Text::CSV;
 
+my $csv = Text::CSV->new({ binary => 1 });
+my $skip_headers = 1;
+my $skipped_headers = 0;
+
+$csv->column_names( qw( date type sort_code account_number description debit credit balance ) );
+
+say '!Type:Bank';
+
+my $fh = \*STDIN;
+
+while (my $row = $csv->getline_hr($fh))
+{
+  if ($skip_headers && !$skipped_headers)
+  {
+    $skipped_headers = 1;
+  }
+  else
+  {
+    say 'D' . $row->{date};
+
+    if ($row->{credit} ne '')
+    {
+      say 'T' . $row->{credit};
+    }
+    else
+    {
+      say 'T-' . $row->{debit};
+    }
+
+    say 'P' . $row->{description};
+
+    say '^';
+  }
+}
+
+close $fh;
